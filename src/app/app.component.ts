84bb2d7241ce0bf79ffd8725651cc448537d2fb1
Anthony Rivas <arivasc@unsa.edu.pe>
2021-07-30 03:39:17 -0500

    Borramos users con la funcion deleteUser.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,4 +35,11 @@ export class AppComponent {
   sayHello() {
     alert("Hola Desde app.component");
   }
+  deleteUser(user:any) {
+    for(let i=0; i<this.users.length; i++) {
+      if(user==this.users[i]) {
+        this.users.splice(i,1);
+      }
+    }
+  }
 }
