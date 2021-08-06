3f1180af1212ee40b47e282a22e1354088432c66
Anthony Rivas <arivasc@unsa.edu.pe>
2021-08-05 20:39:46 -0500

    Data Binding en formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  fullName : string = "Anthony Rivas";
+  age : number = 25;
   users = ['ryan','joe','cameron','john'];
   activated = false;
 
